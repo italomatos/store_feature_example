887085e1fa4a82ceb48d8e0bc685163a3c99f4d1
Italo Matos <italomatos@gmail.com>
2024-06-20 22:04:27 -0300

    add favorite color

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -64,6 +64,6 @@ class UsersController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def user_params
-    params.require(:user).permit(:name, :receive_newsletter)
+    params.require(:user).permit(:name, :receive_newsletter, :favorite_color)
   end
 end
